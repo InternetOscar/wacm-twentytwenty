add06ee765e5fbfeccb3b744669b58997b9f2ace
internetoscar <oscardobsonbrown@outlook.com>
2020-03-03 20:01:48 +0800

    added some new files

---
+++ api's/prac api.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+
+# link that we are calling for our API program
+link = 'http://wttr.in/Perth?format=3'
+
+# open the link
+wget = urlopen(link)
+
+# read the info (to ram?)
+webtext = wget.read()
+
+# decode text and output
+print(webtext.decode('utf-8'))
