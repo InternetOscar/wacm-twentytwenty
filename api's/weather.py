af894604e368c44b108b796486c26cd0a4a6e27e
internetoscar <oscardobsonbrown@outlook.com>
2020-03-05 13:18:23 +0800

    new mathod, based on the digital ocean article

--- api's/weather.py
+++ api's/weather.py
@@ -1,18 +1,50 @@
-from urllib.request import urlopen
-from secrets import *
 import json
+import requests
+from secrets import *
 
-print(key)
+api_token = '4bb350c97e1bef2ceaa09a2b9841ca8e'
+api_url_base = 'https://api.openweathermap.org/data/2.5/weather?q=Joondalup,au&units=metric&appid='
 
-# link that we are calling for our API program
-link = 'https://api.openweathermap.org/data/2.5/weather?q=Joondalup,au&units=metric&appid='
 
-# open the link
-wget = urlopen(link + key)
+headers = {'Content-Type': 'application/json',
+    'Authorization': 'Bearer {0}'.format(api_token)}
 
-# read the info (to ram?)
-webtext = wget.read()
+def get_current_temp():
 
-# decode text and output
-print(webtext.decode('utf-8'))
+    api_url = '{0}account'.format(api_url_base)
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code >= 500:
+        print('[!] [{0}] Server Error'.format(response.status_code))
+        return None
+    elif response.status_code == 404:
+        print('[!] [{0}] URL not found: [{1}]'.format(response.status_code,api_url))
+        return None
+    elif response.status_code == 401:
+        print('[!] [{0}] Authentication Failed'.format(response.status_code))
+        return None
+    elif response.status_code == 400:
+        print('[!] [{0}] Bad Request'.format(response.status_code))
+        return None
+    elif response.status_code >= 300:
+        print('[!] [{0}] Unexpected Redirect'.format(response.status_code))
+        return None
+    elif response.status_code == 200:
+        ssh_keys = json.loads(response.content.decode('utf-8'))
+        return ssh_keys
+    else:
+        print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
+    return None
+
+current_temp = get_current_temp()
+
+if current_temp is not None:
+    print("Here's the current temp: ")
+    for k, v in main ['temp'].items():
+        print('{0}:{1}'.format(k, v))
+
+else:
+    print('[!] Request Failed')
+# print(data)
 
